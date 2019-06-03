160c86cc7deaf0e0be90286e04f997dfcbf78183
omgitsgod <omgitsgod@gmail.com>
2019-06-03 14:46:34 -0400

    Add state to handle tabs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Home from './Home'
 import { Tab, Tabs, Typography} from '@material-ui/core'
 
 function App() {
+  let view
+  const [tab, setTab] = useState('Home')
+  switch(tab){
+      case "Home":
+      view = <Home />
+      break
+      case "Schedule":
+      view = <Home />
+      break
+      case "Prices":
+      view = <Home />
+      break
+      case "Location":
+      view = <Home />
+      break
+      case "Bio":
+      view = <Home />
+      break
+      default:
+      view = <Home />
+      break
+    }
+    const handleChange = (event, change) => {
+    setTab(change)
+  }
   return (
     <div className="App">
     <Tabs
-      value={null}
-     onChange={null}
+      value={tab}
+     onChange={handleChange}
       variant="fullWidth"
       indicatorColor="secondary"
       textColor="secondary"
       className="nav"
     >
-      <Tab value={"Projects"} label=<h2>Schedule</h2> />
-      <Tab value={"Projects"} label=<h2>Prices</h2> />
+      <Tab value={"Schedule"} label=<h2>Schedule</h2> />
+      <Tab value={"Prices"} label=<h2>Prices</h2> />
       <Tab value={"Home"} label=<h2>Home</h2> />
-      <Tab value={"Resume"} label=<h2>Location</h2> />
-      <Tab value={"Projects"} label=<h2>Bio</h2> />
+      <Tab value={"Location"} label=<h2>Location</h2> />
+      <Tab value={"Bio"} label=<h2>Bio</h2> />
 
     </Tabs>
     <Home />
